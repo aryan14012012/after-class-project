2851d9682a0270304b555db7bec34ed41ce822ea
aryan14012012 <rayarjun452@gmail.com>
2026-05-18 19:33:40 +0530

    js

--- hello.js
+++ hello.js
@@ -1,2 +1,12 @@
- const name = " aryan   "; 
-  console.log(name.replace("a", "A"))
+
+ 
+
+function trigonometricValues(sin,cos,tan) {
+
+    mathrd=Math.sin(sin * Math.PI / 180);
+    mathrc=Math.cos(cos * Math.PI / 180);   
+    mathrt=Math.tan(tan * Math.PI / 180);
+}
+
+
+console.log(trigonometricValues(45, 46, 82));
