624fd55315c6d40119513d9c3ffdf925dbe87ce0
aryan14012012 <rayarjun452@gmail.com>
2026-05-18 20:16:18 +0530

    age limit

---
+++ hello.py
@@ -0,0 +1,13 @@
+
+print("Eligibility to ride the roller coaster")
+
+a = int(input("Enter your age: "))
+if 18 <= a <= 50:
+    print("You are eligible to ride the roller coaster")
+
+
+
+elif 1 <= a < 17:
+    print("You are not eligible to ride the roller coaster")
+
+
