a666a43e789f2e37d5fa934b60e404f6e0d54c60
aryan14012012 <rayarjun452@gmail.com>
2026-05-19 20:50:45 +0530

    pricing

--- hello.py
+++ hello.py
@@ -1,13 +1,35 @@
-
 print("Eligibility to ride the roller coaster")
 
+
 a = int(input("Enter your age: "))
-if 18 <= a <= 50:
+if 18 <= a < 60:
     print("You are eligible to ride the roller coaster")
 
 
-
-elif 1 <= a < 17:
+else:
     print("You are not eligible to ride the roller coaster")
 
+if 18 <= a < 20:
+    print("The price is $12")
 
+
+    a = int(input("Enter your age: "))
+if 20 <= a < 40:
+    print("You are eligible to ride the roller coaster")
+
+else:
+    print("You are not eligible to ride the roller coaster")
+
+if 20 <= a < 40:
+    print("The price is $20")
+
+a = int(input("Enter your age: "))
+if 40 <= a < 60:
+    print("You are eligible to ride the roller coaster")
+    
+
+else:
+    print("You are not eligible to ride the roller coaster")
+
+if 40 <= a < 60:
+    print("There is no charge for you ")
