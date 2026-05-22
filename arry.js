9f46b09f46c2df7a3468cb5c1accc390f00fac5d
aryan14012012 <rayarjun452@gmail.com>
2026-05-22 20:24:56 +0530

    arry

---
+++ arry.js
@@ -0,0 +1,13 @@
+  //arry
+
+  let a=["aryan","satyarth","satyam","aryan","aryan","satyarth","satyam","aryan"];
+
+
+  console.log(a[5]);
+//transversing 
+
+  for(let i=0;i<a.length;i++){
+    console.log(a[i]);
+  }
+  console.log(a.length);
+  
