1307eefc78063e465764de3b02d388f5eda37b00
aryan14012012 <rayarjun452@gmail.com>
2026-07-10 20:36:58 +0530

    intro of python

---
+++ intro.py
@@ -0,0 +1,12 @@
+print('Hello, World!')
+print(55+21)
+print('This is a simple Python script.')
+      
+print(8852*2202)
+
+a=55
+b=5
+print('add 5 to a variable that is 55:', a+b)
+name='aryan'
+grade=9
+print('Hello, my name is', name, 'and I am in grade', grade, 'th')
